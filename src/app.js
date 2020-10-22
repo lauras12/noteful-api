cccc3da2f35c5db92b40b9f7f40f6dd77ac1f582
Laura Sikora <lauraannsikora@gmail.com>
2020-10-22 15:33:05 -0400

    updated app.js

--- src/app.js
+++ src/app.js
@@ -22,9 +22,19 @@ app.use(cors())
 app.use('/api/folders', foldersRouter)
 app.use('/api/notes', notesRouter)
 
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
       res.send('Hello, world!')
      })
+  */
+
+app.get('/api', (req, res) => {
+    res.send(noteful)
+})
+
+app.get('/', (req, res) => {
+    // res.send(noteful)
+    res.send('Hello, world!')
+})
 
 
 app.use(function errorHandler(error, req, res, next) {
